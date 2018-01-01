9a69fcef1ddf7b6f0316d24c3821ba1d0de3f41b
Priyanka <priyankag@cse.iitb.ac.in>
2018-01-01 17:05:52 +0530

    Added functionality of message box.

--- calc.py
+++ calc.py
@@ -5,7 +5,6 @@ import sys
 
 class Calculator:
     def __init__(self):
-        print "Inside init"
         app = QApplication(sys.argv)
         widget = QWidget()
         widget.resize(600,300)
@@ -15,38 +14,63 @@ class Calculator:
         self.input2.move(100,80)
         clckadd = QPushButton("ADD", widget)
         clckadd.move(400,20)
-        self.clcksub = QPushButton("SUBTRACT", widget)
-        self.clcksub.move(400,70)
-        self.clckmul = QPushButton("MULTIPLY", widget)
-        self.clckmul.move(400,140)
-        self.clckdiv = QPushButton("DIVIDE", widget)
-        self.clckdiv.move(400,210)
+        clcksub = QPushButton("SUBTRACT", widget)
+        clcksub.move(400,70)
+        clckmul = QPushButton("MULTIPLY", widget)
+        clckmul.move(400,140)
+        clckdiv = QPushButton("DIVIDE", widget)
+        clckdiv.move(400,210)
         self.label = QLabel("", widget)
         self.label.move(100,150)
         self.label.setFixedWidth(200)
         widget.show()
         clckadd.clicked.connect(self.addition)
+        clcksub.clicked.connect(self.subtraction)
+        clckmul.clicked.connect(self.multiplication)
+        clckdiv.clicked.connect(self.division)
         sys.exit(app.exec_())
 
     def addition(self):
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
-        self.label.setText("Addition is: "+str(num1+num2))
+        var1 = str(self.input1.text())
+        var2 = str(self.input2.text())
+        if self.check(var1,var2):
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.label.setText("Addition is: "+str(num1+num2))
 
     def subtraction(self):
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
-        self.label.setText("Subtraction is: "+str(num1-num2))
+        var1 = str(self.input1.text())
+        var2 = str(self.input2.text()) 
+        if self.check(var1,var2):
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.label.setText("Subtraction is: "+str(num1-num2))
 
     def multiplication(self):
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
-        self.label.setText("Multiplication is: "+str(num1*num2))
+        var1 = str(self.input1.text())
+        var2 = str(self.input2.text())
+        if self.check(var1,var2):
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.label.setText("Multiplication is: "+str(num1*num2))
 
-    def division():
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
-        self.label.setText("Division is: "+str(num1/num2))
+    def division(self):
+        var1 = str(self.input1.text())
+        var2 = str(self.input2.text())
+        if self.check(var1,var2):
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.label.setText("Division is: "+str(num1/num2))
+   
+    def check(self,var1,var2):
+        if not (var1.isdigit() and var2.isdigit()):
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            self.label.setText(" ")
+            msg.setText("This works only for integer values")
+            ret = msg.exec_()
+            return False
+        return True
 
 
 c=Calculator()
